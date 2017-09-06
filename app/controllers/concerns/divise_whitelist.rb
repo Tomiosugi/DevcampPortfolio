fd6e1f73d006c9ecdd9de3b3109a192e0e976cae
Jordan Hudgens <jordan@devcamp.com>
2017-09-06 23:27:52 +0000

    Completed authentication with virtual attrs for user model

--- app/controllers/concerns/divise_whitelist.rb
+++ app/controllers/concerns/divise_whitelist.rb
@@ -1,5 +1,4 @@
 module DeviseWhitelist
-module DeviseWhitelist
   extend ActiveSupport::Concern
 
   included do
@@ -10,5 +9,4 @@ module DeviseWhitelist
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
-end
 end
